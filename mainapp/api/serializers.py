ed09bebc3d87d1310b09ca0ab8113b05340c5f0e
Bogdan Karmyzow <big_bogdan@rambler.ru>
2021-05-06 18:02:46 +0300

    comments added (on api side)

--- mainapp/api/serializers.py
+++ mainapp/api/serializers.py
@@ -1,17 +1,15 @@
 from rest_framework import serializers
 
-from ..models import Thread, Board
+from ..models import Thread, Board, Comment
 
 
 class BoardSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Board
         fields = '__all__'
 
 
 class BoardDetailSerializer(serializers.ModelSerializer):
-
     posts = serializers.SerializerMethodField()
 
     class Meta:
@@ -23,8 +21,13 @@ class BoardDetailSerializer(serializers.ModelSerializer):
         return ThreadSerializer(Thread.objects.filter(blog_category=obj), many=True).data
 
 
-class ThreadSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
 
+
+class ThreadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Thread
         fields = '__all__'
@@ -33,7 +36,12 @@ class ThreadSerializer(serializers.ModelSerializer):
 class ThreadRetrieveSerializer(serializers.ModelSerializer):
 
     blog_category = BoardSerializer()
+    comments = serializers.SerializerMethodField()
 
     class Meta:
         model = Thread
         fields = '__all__'
+
+    @staticmethod
+    def get_comments(obj):
+        return CommentSerializer(Comment.objects.filter(related_thread=obj), many=True).data
