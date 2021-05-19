2f0a71b40d99a1c3888ac80de5a40cc0108006a0
Bogdan Karmyzow <big_bogdan@rambler.ru>
2021-05-19 21:01:36 +0300

    fix: added comments field for retrieving board and thread lists again

--- mainapp/api/serializers.py
+++ mainapp/api/serializers.py
@@ -1,17 +1,15 @@
 from rest_framework import serializers
 
-from ..models import Thread, Board
+from ..models import Thread, Board, Comment
 
 
 class BoardSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Board
         fields = '__all__'
 
 
 class BoardDetailSerializer(serializers.ModelSerializer):
-
     posts = serializers.SerializerMethodField()
 
     class Meta:
@@ -24,16 +22,31 @@ class BoardDetailSerializer(serializers.ModelSerializer):
 
 
 class ThreadSerializer(serializers.ModelSerializer):
+    comments = serializers.SerializerMethodField()
 
     class Meta:
         model = Thread
         fields = '__all__'
 
+    @staticmethod
+    def get_comments(obj):
+        return CommentSerializer(Comment.objects.filter(related_thread=obj), many=True).data
+
 
 class ThreadRetrieveSerializer(serializers.ModelSerializer):
-
     blog_category = BoardSerializer()
+    comments = serializers.SerializerMethodField()
 
     class Meta:
         model = Thread
+        fields = '__all__'
+
+    @staticmethod
+    def get_comments(obj):
+        return CommentSerializer(Comment.objects.filter(related_thread=obj), many=True).data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
         fields = '__all__'
